d73b95019b70f4b2e803f08ee58bbcc81c827be5
gabrielSBorges <borgesgabrielsilva@gmail.com>
2019-04-02 18:45:06 -0300

    Formulário de cadastro criado

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Teste de Estudo</title>
   </head>
   <body>
-
+    <form class="cadastro" action="validateFront" method="post">
+      <input type="text" id="nome" name="nome" placeholder="Digite seu nome aqui..."><br>
+      <input type="text" id="email" name="email" placeholder="Digite seu email aqui..."><br>
+      <input type="text" id="cpf" name="cpf" placeholder="Digite seu CPF aqui..."><br>
+      <input type="text" id="corFav" name="corFav" placeholder="Digite sua cor favorita aqui..."><br>
+      <button type="submit" name="enviar">ENVIAR</button>
+    </form>
   </body>
 </html>
