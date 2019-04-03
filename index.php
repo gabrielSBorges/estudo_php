a360aead9756054f53c9abb333302e4ee822a086
gabrielsborges <gabriel.borges@rotaexata.com.br>
2019-04-03 13:04:54 -0300

    index e css alterados para o do véi

--- index.php
+++ index.php
@@ -1,17 +1,46 @@
 <!DOCTYPE html>
 <html>
-  <head>
+    
+<head>
     <meta charset="utf-8">
-    <title>Teste de Estudo</title>
-  </head>
-  <body>
-    <h1>Teste Cadastro</h1>
-    <form class="cadastro" action="validateFront" method="post">
-      <input type="text" id="nome" name="nome" placeholder="Digite seu nome aqui..."><br>
-      <input type="text" id="email" name="email" placeholder="Digite seu email aqui..."><br>
-      <input type="text" id="cpf" name="cpf" placeholder="Digite seu CPF aqui..."><br>
-      <input type="text" id="corFav" name="corFav" placeholder="Digite sua cor favorita aqui..."><br>
-      <button type="submit" name="enviar">ENVIAR</button>
-    </form>
-  </body>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Sistema de Login</h3>
+                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
+                    <div class="notification is-danger">
+                      <p>ERRO: Usuário ou senha inválidos.</p>
+                    </div>
+                    <div class="box">
+                        <form action="login.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
 </html>
